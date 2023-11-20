84da2b2e767983f883b05569818e3f1440e5b74b
Julia <60198903+ninjulia@users.noreply.github.com>
2023-11-20 11:50:35 -0500

    add update trello & shop merge, smooth scroll

--- src/_data/combined.js
+++ src/_data/combined.js
@@ -1,24 +1,18 @@
 const shopList = require('./shops');
 const trelloList = require('./trello');
 
-// Combine shop and trello results
-function mergeLists(list1, list2) {
-	const combinedList = [...list1, ...list2];
-
-	// Remove duplicates
-	const mergedList = combinedList.reduce((accumulator, current) => {
-		// Check if the current object already exists in the accumulator
-		const match = accumulator.findIndex((shop) => shop.name === current.name);
-		// If the object already exists, update its offer
-		if (match > -1) {
-			accumulator[match].offer = current.offer;
-		} else {
-			// Otherwise, add the object to the accumulator
-			accumulator.push(current);
-		}
-		return accumulator;
-	}, []);
-	return mergedList;
+//validate trello data, update shopList with any offers
+function addOffers(list1, list2) {
+	//for each listing in Trello array
+	list1.forEach((shop) => {
+		//compare to shop List
+		list2.filter((obj) => {
+			//update offer if name matches
+			if (obj.name === shop.name) obj.offer = shop.offer;
+		});
+	});
+	//return updated shopList data
+	return shopList;
 }
 
 // Sort shops by offer, alpha
@@ -42,13 +36,12 @@ function sortList(list) {
 }
 
 async function obtainTrello() {
-	//trelloData = await trelloList().then((res) => res)
 	return trelloList();
 }
 
 module.exports = async () => {
 	const trelloData = await obtainTrello();
-	const mergedList = mergeLists(shopList, trelloData);
+	const mergedList = addOffers(trelloData, shopList);
 	const sortedList = sortList(mergedList);
 	return sortedList;
 };
